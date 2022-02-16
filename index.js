1678722d3f4f14a2ae7c3886e592d7684bb17d14
Nico Picchio <nicolapicchio@gmail.com>
2022-02-16 19:15:41 +0000

    basic exercise completed - todos are rendered and a user can add a new todo

--- index.js
+++ index.js
@@ -1,27 +1,57 @@
 const todoList = document.querySelector('#todo-list');
+const addTodoForm = document.querySelector('.add-todo');
 
 function renderTodo(todo) {
 	const liElement = document.createElement('li');
 	liElement.innerText = todo.title;
-  if (todo.completed) {
-    liElement.setAttribute('class', 'completed')
-  }
-  todoList.append(liElement);
+	if (todo.completed) {
+		liElement.setAttribute('class', 'completed');
+	}
+	todoList.append(liElement);
 }
 
 function renderTodoList(todos) {
-  todos.forEach(todo => renderTodo(todo));
+	todos.forEach((todo) => renderTodo(todo));
+}
+
+function listenToAddTodo() {
+	addTodoForm.addEventListener('submit', function (e) {
+		e.preventDefault();
+		const todo = {
+			title: addTodoForm.title.value,
+      completed: false
+		};
+		fetch('http://localhost:3000/todos', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(todo),
+		})
+			.then(function (response) {
+				return response.json();
+			})
+			.then(function (todo) {
+				renderTodo(todo);
+			});
+		addTodoForm.reset();
+	});
+}
+
+function init() {
+  listenToAddTodo()
+  getTodos()
 }
 
 function getTodos() {
-  todoList.innerHTML = ''
-  fetch('http://localhost:3000/todos')
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (todos) {
-    renderTodoList(todos);
-  });
+	todoList.innerHTML = '';
+	fetch('http://localhost:3000/todos')
+		.then(function (response) {
+			return response.json();
+		})
+		.then(function (todos) {
+			renderTodoList(todos);
+		});
 }
 
-getTodos()
+init();
